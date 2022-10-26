44ecdc17ab6fd91dd399749de41e5e8b6171bd4b
배윤석-닷 <yoonseok.bae98@dotincorp.com>
2022-10-26 18:32:59 +0900

    초기화면 HelloWorld.vue 에서 index.vue로 변경

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+      <index />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/index.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Index,
   },
 
   data: () => ({
---
+++ src/components/index.vue
@@ -0,0 +1,126 @@
+<template>
+  <!-- <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="logo"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to the Vuetify 3 Beta
+        </h1>
+
+          <h4>Vite Preview</h4>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-5">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-5">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-5">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container> -->
+  <v-app ref="app">
+    <v-app-bar color="grey-lighten-2" name="app-bar" class="justify-center">
+      <div class="d-flex justify-center align-center w-100">
+        <v-btn @click="print('app-bar')">Get data</v-btn>
+      </div>
+    </v-app-bar>
+    <v-navigation-drawer color="grey-darken-2" permanent name="drawer">
+      <div class="d-flex justify-center align-center h-100">
+        <v-btn @click="print('drawer')">Get data</v-btn>
+      </div>
+    </v-navigation-drawer>
+    <v-main>
+      <v-card height="200px"></v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import logo from '../assets/logo.svg'
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    layout: null,
+  }),
+  methods: {
+    print(key) {
+      alert(JSON.stringify(this.$refs.app.getLayoutItem(key), null, 2));
+    },
+  },
+}
+</script>
